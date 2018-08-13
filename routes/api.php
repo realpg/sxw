10ee107790d229a9f833a4bc3dd89247eb3e63d4
zhangli161 <874570038@qq.com>
2018-08-13 17:43:24 +0800

    分支测试

--- routes/api.php
+++ routes/api.php
@@ -57,7 +57,6 @@ Route::group(['middleware' => ['checkXCXToken']], function () {
 	Route::get('myFavorite', 'CommentController@myFavorite');//我的收藏
 	
 	Route::post('member/editInfo', 'CompanyController@edit');//升级为会员
-//	Route::post('member/updateInfo', 'CompanyController@update');//升级为会员
 	Route::get('member/editInfo', 'CompanyController@getEditInfo');
 	Route::post('member/clockin', 'ClockinController@clockin');//签到
 	Route::get('member/clockin/history', 'ClockinController@getHistory');//签到记录
