9473ba646740e2256f953c31f1db42d35c2b2743
zhangli161 <874570038@qq.com>
2018-08-25 17:18:37 +0800

    删除

--- routes/web.php
+++ routes/web.php
@@ -10,10 +10,7 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
-//
-//Route::get('/', function () {
-//    return view('welcome');
-//});
+
 Route::get('log', 'DemoController@log');
 Route::get('test', 'DemoController@test');
 
