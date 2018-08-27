adbbca4aa73443819644188c8e0404656d67e0b1
zhangli161 <874570038@qq.com>
2018-08-27 10:30:37 +0800

    修复

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -15,8 +15,9 @@ use Illuminate\Http\Request;
 
 class LoginController extends Controller
 {
-	private $AppId = "wxd80c70c308c99008";
-	private $AppSecret = 'f5d639500976d9d0604c36b2cb08a6d5';
+	//客户提供
+	private $AppId = "wx3c1f8dfde816c48f";
+	private $AppSecret = '95006aa78e8f4f7c146d0654a2bf0e55';
 	
 	//登录页面
 	public function getOpenid(Request $request)
