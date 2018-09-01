103e1363334add9ef9d33224929316d592c030fe
zhangli161 <874570038@qq.com>
2018-09-01 11:41:01 +0800

    git

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -17,10 +17,10 @@ use Illuminate\Http\Request;
 
 class LoginController extends Controller
 {
-	private $AppId = "wx3c1f8dfde816c48f";
-	private $AppSecret = '95006aa78e8f4f7c146d0654a2bf0e55';
+	private $AppId = "wxd80c70c308c99008";
+	private $AppSecret = 'f5d639500976d9d0604c36b2cb08a6d5';
 	
-	//登录页面
+	//登录页面111
 	public function getOpenid(Request $request)
 	{
 		$data = $request->all();
