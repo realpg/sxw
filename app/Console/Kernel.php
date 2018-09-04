83b650d057313c7679628c1fe043cac8e5bc2d4f
zhangli161 <874570038@qq.com>
2018-09-04 13:36:20 +0800

    跑吧跑吧

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -35,10 +35,10 @@ class Kernel extends ConsoleKernel
 			RankingController::createDailyRanking(1);
 		})->everyThirtyMinutes()->hourlyAt(30);
 		
-//		$schedule->call(function () {
-//			//每分钟生成日榜
-//			RankingController::createDailyRanking(1);
-//		})->everyMinute();
+		$schedule->call(function () {
+			//每分钟生成日榜
+			RankingController::createDailyRanking(1);
+		})->everyMinute();
 		
 		$schedule->call(function () {
 			//每小时生成周排行榜
