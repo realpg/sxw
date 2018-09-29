b088dcbe502a16bac2532934b57b5d4a567cf64c
zhangli161 <874570038@qq.com>
2018-09-29 10:05:12 +0800

    111

--- resources/views/member/edit.blade.php
+++ resources/views/member/edit.blade.php
@@ -45,8 +45,9 @@
         <div class="row cl ">
             <label class="form-label col-xs-4 col-sm-2">用户id：</label>
             <div class="formControls col-xs-8 col-sm-9">
-                <input type="text" class="input-text" value="{{array_get($data,'userid')}}" placeholder=""
-                       id="userid" name="userid">
+                <input type="text" class="input-text hidden" value="{{array_get($data,'userid')}}" placeholder=""
+                       id="userid" name="userid" >
+                {{array_get($data,'userid')}}
             </div>
         </div>
 
