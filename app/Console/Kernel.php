6f9ca6f7e65a2a103e89f0b65a64b5e9db98b4d5
zhangli161 <874570038@qq.com>
2018-10-11 16:13:35 +0800

    修复

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -40,11 +40,14 @@ class Kernel extends ConsoleKernel
 			//每30分钟生成日榜
 			RankingController::createDailyRanking(1);
 		})->everyThirtyMinutes()->hourlyAt(30);
-
+		
 		$schedule->call(function () {
 			//每分钟同步积分
-//			We7Controller::syncCreditRecordFromWe7();
-//			We7Controller::syncCreditToWe7();
+			$m = (int)date('i');
+			if ($m % 2)
+				We7Controller::syncCreditRecordFromWe7();
+			else
+				We7Controller::syncCreditToWe7();
 		})->everyMinute();
 		
 		$schedule->call(function () {
@@ -68,18 +71,18 @@ class Kernel extends ConsoleKernel
 				if (!$user) {
 					$sell->delete();
 				} else {
-					if($sell->thumbs==''){
-						$sell->thumbs.=$sell->thumb;
-						$sell->thumbs=$sell->thumbs?($sell->thumbs.','.$sell->thumb1):$sell->thumbs;
-						$sell->thumbs=$sell->thumbs?($sell->thumbs.','.$sell->thumb2):$sell->thumbs;
+					if ($sell->thumbs == '') {
+						$sell->thumbs .= $sell->thumb;
+						$sell->thumbs = $sell->thumbs ? ($sell->thumbs . ',' . $sell->thumb1) : $sell->thumbs;
+						$sell->thumbs = $sell->thumbs ? ($sell->thumbs . ',' . $sell->thumb2) : $sell->thumbs;
 						$sell->save();
 					}
 					
 					$searchInfo = SellManager::createSearchInfo($sell);
 					$searchInfo->save();
 					
-					$infodata=SellDataManager::getById($sell->itemid);
-					InfoController::Info_Banword($sell,$infodata);
+					$infodata = SellDataManager::getById($sell->itemid);
+					InfoController::Info_Banword($sell, $infodata);
 				}
 			}
 			$buys = BuyManager::getList();
@@ -88,17 +91,17 @@ class Kernel extends ConsoleKernel
 				if (!$user) {
 					$buy->delete();
 				} else {
-					if($buy->thumbs==''){
-						$buy->thumbs.=$buy->thumb;
-						$buy->thumbs=$buy->thumbs?($buy->thumbs.','.$buy->thumb1):$buy->thumbs;
-						$buy->thumbs=$buy->thumbs?($buy->thumbs.','.$buy->thumb2):$buy->thumbs;
+					if ($buy->thumbs == '') {
+						$buy->thumbs .= $buy->thumb;
+						$buy->thumbs = $buy->thumbs ? ($buy->thumbs . ',' . $buy->thumb1) : $buy->thumbs;
+						$buy->thumbs = $buy->thumbs ? ($buy->thumbs . ',' . $buy->thumb2) : $buy->thumbs;
 						$buy->save();
 					}
 					$searchInfo = BuyManager::createSearchInfo($buy);
 					$searchInfo->save();
 					
-					$infodata=BuyDataManager::getById($buy->itemid);
-					InfoController::Info_Banword($buy,$infodata);
+					$infodata = BuyDataManager::getById($buy->itemid);
+					InfoController::Info_Banword($buy, $infodata);
 				}
 			}
 			$fjmys = FJMYManager::getList();
@@ -107,17 +110,17 @@ class Kernel extends ConsoleKernel
 				if (!$user) {
 					$fjmy->delete();
 				} else {
-					if($fjmy->thumbs==''){
-						$fjmy->thumbs.=$fjmy->thumb;
-						$fjmy->thumbs=$fjmy->thumbs?($fjmy->thumbs.','.$fjmy->thumb1):$fjmy->thumbs;
-						$fjmy->thumbs=$fjmy->thumbs?($fjmy->thumbs.','.$fjmy->thumb2):$fjmy->thumbs;
+					if ($fjmy->thumbs == '') {
+						$fjmy->thumbs .= $fjmy->thumb;
+						$fjmy->thumbs = $fjmy->thumbs ? ($fjmy->thumbs . ',' . $fjmy->thumb1) : $fjmy->thumbs;
+						$fjmy->thumbs = $fjmy->thumbs ? ($fjmy->thumbs . ',' . $fjmy->thumb2) : $fjmy->thumbs;
 						$fjmy->save();
 					}
 					$searchInfo = FJMYManager::createSearchInfo($fjmy);
 					$searchInfo->save();
 					
-					$infodata=FJMYDataManager::getById($fjmy->itemid);
-					InfoController::Info_Banword($fjmy,$infodata);
+					$infodata = FJMYDataManager::getById($fjmy->itemid);
+					InfoController::Info_Banword($fjmy, $infodata);
 				}
 			}
 		})->dailyAt('19:00');  //unix时间19点，即北京时间3点
